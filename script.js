340898e8067cbab7a58773d2389e26afea880357
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-03-05 14:35:41 +0300

    debut js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let panierCounter = 0 ;
+const plus = document.getElementsByClassName(plus);
+const moin = document.getElementsByClassName(moin);
 
+plus.addEventListener('click', () => {
+    panierCounter++;
+});
+moin.addEventListener('click', () => {
+    panierCounter--;
+});
